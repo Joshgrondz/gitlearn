772de79df277231644d9bdcc4afe422f4177a842
Augustine Mucci <139827040+augmucci@users.noreply.github.com>
2023-10-20 10:14:17 -0400

    My stuff

--- prog.java
+++ prog.java
@@ -6,13 +6,6 @@ public class prog
 		System.out.println("hello there");
 		System.out.println("GobStop");
 		System.out.println("Josh is a stupider head");
-		System.out.println("Test");daf
-
-			adf
-			adsf
-					ad
-					fad
-							fad
-							f
+		System.out.println("Test");
 	}
 }
